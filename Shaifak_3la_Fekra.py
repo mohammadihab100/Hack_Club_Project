fd267a76db45ba691f3e070ac9d55546ee1c07a6
mohammadihab100 <103046571+mohammadihab100@users.noreply.github.com>
2022-09-09 21:40:14 +0200

    Add files via upload

---
+++ Shaifak_3la_Fekra.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    strenghts = [int(s) for s in input().split()]
+
+    minimum = min(strenghts)
+    maximum = max(strenghts)
+
+    count = 0
+
+    for i in range(n):
+        if minimum < strenghts[i] < maximum:
+            count += 1
+    print(count)
